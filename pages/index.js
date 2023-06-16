0547a40a5b983a791e57e44848321a8176f4af1e
sathish19961412 <sathishfreelanc5@gmail.com>
2023-06-16 13:36:49 +0530

    Completed to The Basic Details of Two Sections

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from 'react-icons/ai'
 import { Inter } from '@next/font/google'
-
+import code from '../public/code.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +27,41 @@ export default function Home() {
                   <li className='text-xl text-blue-500 hover:text-green-500 cursor-pointer'>Resume</li>
                 </ul>
               </nav>
+              <div className='text-center p-10'>
+                 <h2 className='text-5xl py-5 text-teal-600'>Sathish</h2>
+                 <h3 className='text-2xl py-2'>FrontEnd Developer</h3>
+                 <p className='text-md py-5  font-medium leading-8 text-gray-600'>
+                   It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                   It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.  
+                 </p>
+              </div>
+              <div className='text-6xl flex justify-center gap-16 text-blue-600'>
+                <AiFillLinkedin />
+                <AiFillTwitterCircle  />
+                <AiFillYoutube />
+              </div>
+          </section>
+
+          <section className='py-11'>
+             <div>
+                <h3 className='text-3xl py-1 text-green-700 font-medium'>Services I Offer</h3>
+                <p className='text-md py-2 leading-8 text-gray-700'>
+                  There are many variations of passages of Lorem Ipsum available, but the majority have suffered <span className='text-green-800 hover:text-red-500 cursor-pointer'>alteration </span>in some form, by injected humour, or randomised words which don't look even slightly believable. 
+                </p>
+                <p className='text-md py-2 leading-8 text-gray-700'>
+                  There are <span className='text-green-800 hover:text-red-500 cursor-pointer'>many variations</span> of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
+                </p>
+             </div>
+             <div>
+               <div>
+                  <Image src={code} />
+                  <h3>Coding Language</h3>
+                  <p>
+                    There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
+                  </p>
+                  
+               </div>
+             </div>
           </section>
       </main>
     </>
